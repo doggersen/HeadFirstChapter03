7ca6d5ff71271c44a2b2c7ec74e7db76d11bc184
doggersen <doggersen@gmail.com>
2021-05-04 22:36:27 +0200

    more changes

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -9,14 +9,14 @@ namespace PickRandomCards
             Console.Write("Enter the number of cards to pick: ");
             //assigns the user input to a string variable
             string line = Console.ReadLine();
-            //if user input can be converted int then do the following..
-            if (int.TryParse(line, out int numberOfCards))
+            //if user input can be converted to int then do the following..
+            if (int.TryParse(line, out int numberCards))
             {
                                                                                     //My own solution (the solution from the book was simpler):
                                                                                     //string[] results = CardPicker.PickSomeCards(numberOfCards);
                                                                                     //int count = 0;
                 //calling the PickSomeCards method, and inputting the user input
-                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                foreach (string card in CardPicker.PickSomeCards(numberCards))
                 {
                     Console.WriteLine(card);
 
