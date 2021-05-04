49350b9631e0be661baf8fc3a803e32d88807017
doggersen <doggersen@gmail.com>
2021-05-04 07:44:16 +0200

    random exercise done

---
+++ SharpenYourPencil_Random/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SharpenYourPencil_Random
+{
+    class Program
+    {
+
+        /*
+            The methods of the Random class:
+            Equals
+            GetHashCode
+            GetType
+            Next
+            NextDoubles
+            NextBytes
+            ToString
+            
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Random random = new Random();
+            double[] randomDoubles = new double[20];
+
+            for(int i = 0; i < 20; i++ )
+            {
+                double value = random.NextDouble();
+                randomDoubles[i] = value;
+
+                Console.WriteLine(randomDoubles[i].ToString());
+
+
+            }
+
+        }
+    }
+}
