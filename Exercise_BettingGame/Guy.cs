79940eb30de0352988738d1f8e23cc21b33d773e
doggersen <doggersen@gmail.com>
2021-05-05 22:02:35 +0200

    almost finished with chap3

---
+++ Exercise_BettingGame/Guy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Exercise_BettingGame
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + Cash + " bucks.");
+        }
+
+        /// <summary>
+        /// Gives some of my cash, removing it from my wallet (or printing
+        /// a message to the console if I don't have enough cash).
+        /// </summary>
+        /// <param name="amount">Amount of cash to give.</param>
+        /// <returns>The amount of cash removed from my wallet,
+        /// or 0 if I don't have enough cash (or if the amount is invalid).</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
+                return 0;
+
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " says: " + " I don't have enough cash to give you " + amount);
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+        /// <summary>
+        /// Receive some cash, adding it to my wallet (or printing
+        /// a message to the console if the amount is invalid).
+        /// </summary>
+        /// <param name="amount">Amount of cash to give(shouldn't it say RECIEVE instead?!?!</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't an amouny I'll take");
+
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
---
+++ Exercise_BettingGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise_BettingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            Guy player = new Guy() { Name = "The Player", Cash = 100 };
+
+            Console.WriteLine("welcome player, to the betting game! the odds are 0.75");
+
+            while (true)
+            {
+                Console.WriteLine("The Player has: " + player.Cash + " bucks");
+                Console.WriteLine("How much money do you want to bet?");
+                string howMuch = Console.ReadLine();
+
+                if(int.TryParse(howMuch, out int amount))
+                {                    
+                    int pot = amount *2 ;
+
+                    double randomNumber = random.NextDouble();
+                    if (randomNumber > odds)
+                    {
+                        player.ReceiveCash(amount);
+                    }
+                    else
+                    {
+                        player.GiveCash(amount);
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
